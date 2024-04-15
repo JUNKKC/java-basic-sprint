bd00dcffcbab12ad7a29afa3beacfa22fec08d21
kimchanjun <160035676+kimmamam@users.noreply.github.com>
2024-04-15 11:11:31 +0900

    Add files via upload

--- src/f_2.java
+++ src/f_2.java
@@ -1,6 +1,8 @@
 public class f_2 {
     public static void main(String[] args) {
 
+        System.out.println(StringUtil.concat("132","sfafa","asfasf"));
+        System.out.println(StringUtil.contains("abc","cabc"));
 
 
     }
